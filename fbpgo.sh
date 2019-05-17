c64879dc4dda4335bffb48e54ee76f96f5442ad2
tyoung3 <tyoung3@taos.twyoung.com>
2019-05-17 00:44:26 -0400

    Fix bad name goimp*go

--- fbpgo.sh
+++ fbpgo.sh
@@ -95,6 +95,21 @@ NotesFBP() {
 	* Replace Launch4
 	* Collate component
 	
+##GitHub Flow
+
+https://githubflow.github.io/	
+	
+	* Anything in the master branch is deployable
+	* To work on something new, create a descriptively named branch 
+	  off of master (ie: new-oauth2-scopes)
+	* Commit to that branch locally and regularly push your work 
+	  to the same named branch on the server
+	* When you need feedback or help, or you think the branch is 
+	  ready for merging, open a pull request
+	* After someone else has reviewed and signed off on the feature, 
+	  you can merge it into master
+	* Once it is merged and pushed to 'master', you can and 
+	  should deploy immediately
 	
 	
 #$pgm
